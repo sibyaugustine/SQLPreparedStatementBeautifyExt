905559423f8e872e1f0d61206b727a4a647e81b6
siby <sibyaugustine93@gmail.com>
2017-06-26 22:54:23 +0530

    Added Background script

---
+++ ext_specific/scripts/background.js
@@ -0,0 +1,14 @@
+/**
+ * Listens for the app launching, then creates the window.
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.create({
+        'url': chrome.extension.getURL('core/index.html')
+    }, function(tab) {
+
+    });
+});
